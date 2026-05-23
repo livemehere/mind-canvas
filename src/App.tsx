92b2fc4511f67383101be5595e32ff374bde8bd2
livemehere <rhdxoals@gmail.com>
2026-05-23 19:22:26 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { cn } from "./utils/cn";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className={cn("text-sm text-red-500")}>Hello World</h1>
+    </div>
+  );
+}
